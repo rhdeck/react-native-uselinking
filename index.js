da13adcb746e1381dba2d61fe39f04e23852550c
Ray Deck <ray@raydeck.com>
2020-04-09 13:01:16 -0500

    Adding provider for making sure there is only one of these out there.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 import { Linking } from "react-native";
 const useLinking = () => {
   const [url, setUrl] = useState(null);
@@ -18,4 +18,16 @@ const useLinking = () => {
 
   return url;
 };
+const context = createContext();
+const { Provider } = context;
+const LinkingProvider = ({ children }) => {
+  const value = useLinking();
+  return <Provider value={value}>{children}</Provider>;
+};
+
+const useLinkingProvider = () => {
+  const url = useContext(context);
+  return url;
+};
 export default useLinking;
+export { LinkingProvider, useLinkingProvider as useLinking };
